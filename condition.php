bd9f7c845f9754a7a7b5b12c1bd958a19a7ef2ac
Oleg9791 <oleg9791@tut.by>
2021-03-12 18:48:05 +0300

    added bootstraps

--- condition.php
+++ condition.php
@@ -1,7 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Document</title>
+</head>
+<body>
 <?php
 // Проверка на хотя бы одного невыбранного варианта ответа на вопрос и возможностью вернуться к опросу
 if (empty($_POST["q1"]) or empty($_POST["q2"]) or empty($_POST["q3"]) or empty($_POST["q4"])
     or empty($_POST["q5"])) {
-    echo "Выберите варианты ответа для всех вопросов <br> <a href='opros.php'>Вернуться к опросу</a>";
-
+    echo "Выберите варианты ответа для всех вопросов <br> <a class='btn btn-secondary' href='opros.php'>Вернуться к опросу</a>";
 }
+?>
+</body>
+</html>
+
